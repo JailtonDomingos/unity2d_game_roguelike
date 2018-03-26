cd10ade6ffe018b9468a6baa2305d56b68e76799
JailtonDomingos <development.j@outlook.com>
2018-03-25 23:28:15 -0300

    commit

---
+++ Assets/Scripts/character.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class character : MonoBehaviour {
+
+    public Animator animator;
+
+    [SerializeField]
+    private float speed;
+    
+    protected Vector2 direction;
+
+    // Use this for initialization
+    void Start () {
+        animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	protected virtual void Update () {
+        move();
+    }
+
+    public void move(){
+        transform.Translate(direction * speed * Time.deltaTime);
+
+        AnimateMovement(direction);
+
+    }
+    public void AnimateMovement(Vector2 direction){
+        animator.SetFloat("X", direction.x);
+        animator.SetFloat("Y", direction.y);
+    
+    }
+
+}
